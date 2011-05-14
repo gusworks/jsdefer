65a5c4d95c7565c28ffe47af9bdf5c4fb101ff1f
Plinio Balduino <pbalduino+github@gmail.com>
2011-05-14 20:59:14 -0300

    Added code to refer and render JS

--- lib/jsdefer.rb
+++ lib/jsdefer.rb
@@ -1,8 +1,37 @@
-puts "-------> included <----------"
+class JsDefer
+  @tag_list = []
+
+  def initialize
+    @tag_list = []
+  end
+  
+  def push(tag)
+    @tag_list << tag
+  end
+  
+  def flush
+    ret = ""
+    @tag_list.each do |tag|
+      ret += tag
+    end
+    
+    @tag_list = []
+    
+    ret
+  end
+
+end
+
+defer = JsDefer.new
+
 module ActionView
   class Base
     def defer(tag)
-      puts "------> ok <-------------"
+      defer.push tag
+    end
+    
+    def render_js
+      defer.flush
     end
   end
 end
